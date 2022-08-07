3f0a486736d0c58e434ff8b2dd562960257f8d9d
Jeffrey Omondi <11554jeff@gmail.com>
2022-08-08 00:18:42 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductDetail from "./containers/productDetail";
+import ProductListing from "./containers/productListing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
